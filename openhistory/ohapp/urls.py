c4cafa47f643c6c277ab5fb340a663480992ecb3
aw18 <wong.albertina@gmail.com>
2014-03-02 00:38:28 -0800

    Added base.html. Modified index.html and team.html to inherit from base.html.

--- openhistory/ohapp/urls.py
+++ openhistory/ohapp/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls import patterns, url
 from ohapp import views
 
 urlpatterns = patterns('',
+    url(r'^base$', views.base, name="base"),
     url(r'^$', views.index, name="index"),
     url(r'^p1$', views.p1, name="p1"),
     url(r'^team$', views.team, name="team"),
