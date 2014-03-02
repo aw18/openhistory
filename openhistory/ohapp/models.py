a3eb1abcc8c2d15d5ba70889232631a5a612937b
aw18 <wong.albertina@gmail.com>
2014-03-01 23:36:42 -0800

    Added Photos Model in models.py

--- openhistory/ohapp/models.py
+++ openhistory/ohapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Photos(models.Model):
+    mikan_number = models.IntegerField()
+    title = models.CharField(max_length=500)
+    image_number = CharField(max_length=30)
+    image_url = models.URLField(max_length=200)
+    access_condition = models.CharField(max_length=1000)
+    branch_service = models.CharField(max_length=1000)
+    date_issued = models.DateTimeField()
+    description = models.TextField()
+    location = models.CharField()
+    photographer = models.CharField()
+    subject = models.CharField()
