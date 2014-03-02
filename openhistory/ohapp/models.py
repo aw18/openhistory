56034190253473adbda8304240ae386c6a415bb3
aw18 <wong.albertina@gmail.com>
2014-03-01 23:45:42 -0800

    Syntax error in Models. I fixed it now.

--- openhistory/ohapp/models.py
+++ openhistory/ohapp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Photos(models.Model):
     mikan_number = models.IntegerField()
     title = models.CharField(max_length=500)
-    image_number = CharField(max_length=30)
+    image_number = models.CharField(max_length=30)
     image_url = models.URLField(max_length=200)
     access_condition = models.CharField(max_length=1000)
     branch_service = models.CharField(max_length=1000)
